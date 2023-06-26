cd0b81cf2af9e07b86166def10abd3d6fa5ae9ed
GoldilocksKang <ceo5584@gmail.com>
2023-06-26 14:27:03 +0900

    초기화

---
+++ Source/SummerPrject_2023/GameModeBase_1.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GameModeBase_1.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SUMMERPRJECT_2023_API AGameModeBase_1 : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
